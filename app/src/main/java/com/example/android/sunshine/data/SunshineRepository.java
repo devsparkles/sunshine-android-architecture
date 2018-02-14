4a4d25bb8f200692e60ea0c929ba5c7c8b23ae0a
Xavier MAXIMIN <xavier.maximin.uk@gmail.com>
2018-02-14 15:15:42 +0000

    final Step 16 Backgound Jobs

--- app/src/main/java/com/example/android/sunshine/data/SunshineRepository.java
+++ app/src/main/java/com/example/android/sunshine/data/SunshineRepository.java
@@ -91,6 +91,8 @@ public class SunshineRepository {
         if (mInitialized) return;
         mInitialized = true;
 
+        mWeatherNetworkDataSource.scheduleRecurringFetchWeatherSync();
+
         mExecutors.diskIO().execute(() -> {
             if(isFetchNeeded()) {
                 startFetchWeatherService();
